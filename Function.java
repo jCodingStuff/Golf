43ab3cfc6856c44f4cfbec4b1be5c0897c011ce4
Julián Marrades <julian.marrades@hotmail.es>
2018-03-17 08:37:52 +0100

    Updated gitignore and Function class

--- Function.java
+++ Function.java
@@ -7,6 +7,48 @@
 
 public class Function {
 
-  
+  String name;
+
+  /**
+  * Create a new Function instance
+  * @param name the name of the function
+  */
+  public Function(String name) {
+    this.name = name;
+  }
+
+  /**
+  * Get access to the name of the Function
+  * @return the name of the function
+  */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+  * Set a new name to the function
+  * @param name the new name
+  */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public Function clone() {
+    return new Function(this.name.clone());
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) return false;
+    if (this.getClass() != obj.getClass()) return false;
+    Function other = (Function) obj;
+    return this.name.equals(other.name);
+  }
+
+  @Override
+  public String toString() {
+    return "z = f(x, y) = " + this.name;
+  }
 
 }
