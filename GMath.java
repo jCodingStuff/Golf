7811fb6106b71d5c1795297b5be6d8ac930378e9
Julián Marrades <julian.marrades@hotmail.es>
2018-03-17 08:52:28 +0100

    Added JavaDoc to GMath

--- GMath.java
+++ GMath.java
@@ -4,9 +4,13 @@ import java.util.StringTokenizer;
 * A class with static methods to implement Shunting-Yard Algorithm and
 * RPN computations
 */
-
 public class GMath {
 
+  /**
+  * Transform a string to RPN notation using Shunting-Yard Algorithm
+  * @param formula the input String
+  * @return the input formula in RPN notation
+  */
   public static String toRPN(String formula) {
 
   }
