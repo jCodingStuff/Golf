46e87ff0b12b303eb6cdcd62bed8d52ead555407
Julian <>
2018-03-19 09:04:04 +0100

    Styled intro screen and transition to a blank main menu

--- core/src/com/group/golf/screens/IntroScreen.java
+++ core/src/com/group/golf/screens/IntroScreen.java
@@ -3,9 +3,12 @@ package com.group.golf.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.group.golf.Golf;
 
 import javax.swing.*;
@@ -18,17 +21,45 @@ public class IntroScreen implements Screen {
     Music introMusic;
     Texture introImg;
 
+    FreeTypeFontGenerator titleGenerator;
+    FreeTypeFontGenerator.FreeTypeFontParameter titleParameter;
+    FreeTypeFontGenerator subTitleGenerator;
+    FreeTypeFontGenerator.FreeTypeFontParameter subTitleParameter;
+    BitmapFont titleFont;
+    BitmapFont subTitleFont;
+
     public IntroScreen(final Golf game) {
         this.game = game;
 
+        // Setup cam
         this.cam = new OrthographicCamera();
-        cam.setToOrtho(false, Golf.VIRTUAL_WIDTH, Golf.VIRTUAL_HEIGHT);
+        this.cam.setToOrtho(false, Golf.VIRTUAL_WIDTH, Golf.VIRTUAL_HEIGHT);
 
+        // Setup music
         this.introMusic = Gdx.audio.newMusic(Gdx.files.internal("mariokartds_titlescreen.mp3"));
         this.introMusic.setVolume(0.2f);
         this.introMusic.setLooping(true);
 
+        // Setup background image
         this.introImg = new Texture(Gdx.files.internal("introImg.png"));
+
+        // Setup text
+        this.titleGenerator = new FreeTypeFontGenerator(Gdx.files.internal("GolfToolsFont.ttf"));
+        this.titleParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        this.titleParameter.size = 110;
+        //this.titleParameter.borderWidth = 3;
+        this.titleParameter.shadowColor = Color.BLACK;
+        this.titleParameter.shadowOffsetX = 3;
+        this.titleParameter.shadowOffsetY = 3;
+        this.titleFont = this.titleGenerator.generateFont(this.titleParameter);
+
+        this.subTitleGenerator = new FreeTypeFontGenerator(Gdx.files.internal("MoonGetFont.otf"));
+        this.subTitleParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        this.subTitleParameter.size = 30;
+        this.subTitleParameter.shadowColor = Color.BLACK;
+        this.subTitleParameter.shadowOffsetX = 3;
+        this.subTitleParameter.shadowOffsetY = 3;
+        this.subTitleFont = this.subTitleGenerator.generateFont(this.subTitleParameter);
     }
 
     @Override
@@ -46,12 +77,13 @@ public class IntroScreen implements Screen {
 
         this.game.batch.begin();
         this.game.batch.draw(this.introImg, 0, 0, Golf.VIRTUAL_WIDTH, Golf.VIRTUAL_HEIGHT);
-        this.game.font.draw(this.game.batch, "Crazy Golf", 10, 50);
-        this.game.font.draw(this.game.batch, "Click anywhere to continue", 10, 25);
+        this.titleFont.draw(this.game.batch, "Crazy Golf", 75, 600);
+        this.subTitleFont.draw(this.game.batch, "Click anywhere to continue", 230, 100);
         this.game.batch.end();
 
         if (Gdx.input.isTouched()) {
-            // this.dispose();
+            this.game.setScreen(new MainMenuScreen(this.game));
+            this.dispose();
         }
     }
 
@@ -79,5 +111,9 @@ public class IntroScreen implements Screen {
     public void dispose() {
         this.introMusic.dispose();
         this.introImg.dispose();
+        this.titleGenerator.dispose();
+        this.subTitleGenerator.dispose();
+        this.titleFont.dispose();
+        this.subTitleFont.dispose();
     }
 }
---
+++ core/src/com/group/golf/screens/MainMenuScreen.java
@@ -0,0 +1,72 @@
+package com.group.golf.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.group.golf.Golf;
+
+public class MainMenuScreen implements Screen {
+
+    final Golf game;
+
+    Music menuMusic;
+    OrthographicCamera cam;
+
+    public MainMenuScreen(final Golf game) {
+        this.game = game;
+
+        // Setup cam
+        this.cam = new OrthographicCamera();
+        this.cam.setToOrtho(false, Golf.VIRTUAL_WIDTH, Golf.VIRTUAL_HEIGHT);
+
+        // Setup music
+        this.menuMusic = Gdx.audio.newMusic(Gdx.files.internal("mariokart8_mainmenu.mp3"));
+        this.menuMusic.setVolume(0.2f);
+        this.menuMusic.setLooping(true);
+    }
+
+    @Override
+    public void show() {
+        this.menuMusic.play();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.196f, 0.804f, 0.196f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        this.cam.update();
+        this.game.batch.setProjectionMatrix(this.cam.combined);
+
+        this.game.batch.begin();
+        this.game.font.draw(this.game.batch, "MAIN MENU", 400, 300);
+        this.game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        this.menuMusic.stop();
+    }
+
+    @Override
+    public void dispose() {
+        this.menuMusic.dispose();
+    }
+}
