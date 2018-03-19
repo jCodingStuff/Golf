d182b3922d27460edf05d5067be3391359ea4a3f
Julian <>
2018-03-19 19:24:37 +0100

    Beginning of CourseScreen

---
+++ core/src/com/group/golf/screens/CourseScreen.java
@@ -0,0 +1,77 @@
+package com.group.golf.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.group.golf.Course;
+import com.group.golf.Golf;
+
+public class CourseScreen implements Screen {
+
+    final Golf game;
+    Course course;
+    OrthographicCamera cam;
+    Music music;
+
+    public CourseScreen(final Golf game, Course course) {
+        this.game = game;
+        this.course = course;
+
+        // Setup Cam
+        this.cam = new OrthographicCamera();
+        this.cam.setToOrtho(false, Golf.VIRTUAL_WIDTH, Golf.VIRTUAL_HEIGHT);
+
+        // Setup Music
+        this.music = Gdx.audio.newMusic(Gdx.files.internal("mario64_ost"));
+        this.music.setVolume(0.5f);
+        this.music.setLooping(true);
+
+        // Setup Course
+        this.setUpCourse();
+    }
+
+    private void setUpCourse() {
+        // The center of the screen is the middle point between the start and the goal
+    }
+
+    @Override
+    public void show() {
+        this.music.play();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        this.cam.update();
+        this.game.batch.setProjectionMatrix(this.cam.combined);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        this.music.stop();
+    }
+
+    @Override
+    public void dispose() {
+        this.music.dispose();
+    }
+}
