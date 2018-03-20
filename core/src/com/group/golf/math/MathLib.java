f7a4c1ec5da23fa8625654a2d716c6aa76e1a368
Julian <>
2018-03-20 08:07:38 +0100

    Created MatLib

---
+++ core/src/com/group/golf/math/MathLib.java
@@ -0,0 +1,12 @@
+package com.group.golf.math;
+
+/**
+ * A class to hold static methods for Math purposes
+ */
+public class MathLib {
+
+    public static double map(double value, double min1, double max2, double min2, double max2) {
+        
+    }
+
+}
