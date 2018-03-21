bbfc339f81bc11b3b0795ebe54ffbdc3876f843d
Julian <>
2018-03-21 16:41:57 +0100

    Continuing with the ImportScreen

--- core/src/com/group/golf/listeners/ImportListener.java
+++ core/src/com/group/golf/listeners/ImportListener.java
@@ -1,9 +1,15 @@
 package com.group.golf.listeners;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 import com.group.golf.Golf;
+import com.group.golf.screens.CourseScreen;
 
 public class ImportListener extends ChangeListener {
 
@@ -17,6 +23,13 @@ public class ImportListener extends ChangeListener {
 
     @Override
     public void changed(ChangeEvent event, Actor actor) {
-
+        String name = this.txtField.getText();
+        String path = "courses/" + name + ".json";
+        if (name != "" && Gdx.files.local(path).exists()) {
+            Json json = new Json();
+            FileHandle file = Gdx.files.local(path);
+            String text = file.readString();
+            CourseScreen newCourse = json.fromJson(CourseScreen.class, text);
+        }
     }
 }
