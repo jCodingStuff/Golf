822fb346ba5de8550b914debd77e45655b0ae0c9
lilly <>
2018-03-21 11:51:06 +0100

    textfields for designscreen

--- core/src/com/group/golf/screens/DesignScreen.java
+++ core/src/com/group/golf/screens/DesignScreen.java
@@ -2,11 +2,14 @@ package com.group.golf.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
 import com.group.golf.Golf;
 
 /**
@@ -15,7 +18,11 @@ import com.group.golf.Golf;
 
 
 class DesignScreen implements Screen {
-    private Stage stage;
+    final Golf game;
+
+    Stage stage;
+    OrthographicCamera cam;
+
     TextField txtFunction;
     TextField txtStartPos;
     TextField txtGoalPos;
@@ -29,7 +36,14 @@ class DesignScreen implements Screen {
     TextButton btnMode2;
     ButtonGroup mode;
     public DesignScreen(final Golf game){
+        this.game = game;
         this.stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        // Setup cam
+        this.cam = new OrthographicCamera();
+        this.cam.setToOrtho(false, Golf.VIRTUAL_WIDTH, Golf.VIRTUAL_HEIGHT);
+
         Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
         txtFunction = new TextField("Function", skin);
 
@@ -48,17 +62,28 @@ class DesignScreen implements Screen {
 
         txtFriction.setPosition(300, 400);
         txtFriction.setSize(200,60);
+        txtFriction.addListener(new FocusListener(){
+            public void focusGained(FocusEvent e) {
+                if(txtFriction.getText().equals("Friction "))
+                    txtFriction.setText("");
+            }
 
-        txtFunction.setPosition(300, 400);
+            public void focusLost(FocusEvent e) {
+                if(txtFriction.getText().equals(""))
+                    txtFriction.setText("Friction");
+            }
+        });
+
+        txtFunction.setPosition(300, 300);
         txtFunction.setSize(200,60);
 
-        txtVMax.setPosition(300, 400);
+        txtVMax.setPosition(300, 200);
         txtVMax.setSize(200,60);
 
-        txtStartPos.setPosition(300, 400);
+        txtStartPos.setPosition(300, 100);
         txtStartPos.setSize(200,60);
 
-        txtGoalPos.setPosition(300, 400);
+        txtGoalPos.setPosition(300, 0);
         txtGoalPos.setSize(200,60);
 
         txtGravity.setPosition(300, 400);
@@ -67,11 +92,22 @@ class DesignScreen implements Screen {
         txtRadius.setPosition(300, 400);
         txtRadius.setSize(200,60);
 
+        btnMode1.setPosition(600, 400);
+        btnMode1.setSize(200,60);
+        btnMode2.setPosition(600, 300);
+        btnMode2.setSize(200,60);
+
         mode = new ButtonGroup(btnMode1, btnMode2);
         mode.setMaxCheckCount(1);
         mode.setMinCheckCount(1);
+        mode.setUncheckLast(true);
 
         stage.addActor(txtFriction);
+        stage.addActor(txtFunction);
+        stage.addActor(txtVMax);
+        stage.addActor(btnMode1);
+        stage.addActor(btnMode2);
+
 
         Gdx.input.setInputProcessor(this.stage);
     }
@@ -82,7 +118,14 @@ class DesignScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0.196f, 0.804f, 0.196f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        this.cam.update();
+        this.game.batch.setProjectionMatrix(this.cam.combined);
+
+        stage.act(delta);
+        stage.draw();
     }
     @Override
     public void resize(int width, int height) {
