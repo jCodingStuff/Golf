5460f05db48c73fac9be96b8670675dd05bd85c0
kim <kimroggenbuck@gmail.com>
2018-03-23 13:55:03 +0100

    fixed play and import button in courseselectorscreen

--- core/src/com/group/golf/screens/CourseSelectorScreen.java
+++ core/src/com/group/golf/screens/CourseSelectorScreen.java
@@ -82,6 +82,8 @@ public class CourseSelectorScreen implements Screen {
                 Function function = new Function(formula);
                 Course course = new Course(function, 9.81, 0.5, 3, start, goal, 0.5);
                 design.setTouchable(Touchable.disabled);
+                importbtn.setTouchable(Touchable.disabled);
+                play.setTouchable(Touchable.disabled);
                 this.game.setScreen(new CourseScreen(this.game, course, new Ball(1)));
 
                 this.screen.dispose();
