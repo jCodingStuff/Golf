29028e26a9f7a8d52ec380145d11aa87ba26c9e3
Julian Marrades <julian.marrades@hotmail.es>
2018-04-11 09:41:05 +0200

    Rethinking the collisions system

--- core/src/com/group/golf/Physics/Collision.java
+++ core/src/com/group/golf/Physics/Collision.java
@@ -11,16 +11,21 @@ public class Collision {
     private final Ball ball;
     private final Course course;
 
+    private double lastX;
+    private double lastY;
+
     public Collision(Ball ball, Course course) {
         this.ball = ball;
         this.course = course;
+        this.lastX = this.course.getStart()[0];
+        this.lastX = this.course.getStart()[1];
     }
 
     public boolean isGoalAchieved() {
         double xToGoal = this.course.getGoal()[0] - this.ball.getX();
         double yToGoal = this.course.getGoal()[1] - this.ball.getY();
         double distToGoal = Math.sqrt(Math.pow(xToGoal, 2) + Math.pow(yToGoal, 2));
-        return distToGoal <= this.course.getTolerance();
+        return distToGoal < this.course.getTolerance();
     }
 
     public void checkForWalls(double ballX, double ballY) {
