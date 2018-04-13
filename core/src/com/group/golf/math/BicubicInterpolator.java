554daaf81aec611db434f07bb0f73adfdfced342
Julian Marrades <julian.marrades@hotmail.es>
2018-04-13 12:40:41 +0200

    ADvances

--- core/src/com/group/golf/math/BicubicInterpolator.java
+++ core/src/com/group/golf/math/BicubicInterpolator.java
@@ -2,6 +2,6 @@ package com.group.golf.math;
 
 public class BicubicInterpolator {
 
-
+    private static final double[][] A = new double[][]{};
 
 }
--- core/src/com/group/golf/math/Interpolator.java
+++ core/src/com/group/golf/math/Interpolator.java
@@ -2,8 +2,6 @@ package com.group.golf.math;
 
 public class Interpolator {
 
-    private final static double[][] A = new double[][]{};
-
     public static BicubicInterpolator[][] getInterpolators(Point3D[][] points, double[][] dx, double[][] dy, double[][] dxy) {
         int xUnits = points.length;
         int yUnits = points[0].length;
