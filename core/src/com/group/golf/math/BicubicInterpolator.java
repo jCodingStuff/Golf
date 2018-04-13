c45a9a8d7a4a360b2021587f32a375a83d9a6437
Julian Marrades <julian.marrades@hotmail.es>
2018-04-13 14:29:59 +0200

    Added constant matrices

--- core/src/com/group/golf/math/BicubicInterpolator.java
+++ core/src/com/group/golf/math/BicubicInterpolator.java
@@ -2,7 +2,14 @@ package com.group.golf.math;
 
 public class BicubicInterpolator {
 
-    private static final double[][] A = new double[][]{};
+    private static final double[][] A = new double[][]{{1, 0, 0, 0},
+                                                       {0, 0, 1, 0},
+                                                       {-3, 3, -2, 1},
+                                                       {2, -2, 1, 1}};
+    private static final double[][] B = new double[][]{{1, 0, 0, 0},
+                                                       {0, 0, 1, 0},
+                                                       {-3, 3, -2, 1},
+                                                       {2, -2, 1, 1}};
     
     public BicubicInterpolator() {}
 
