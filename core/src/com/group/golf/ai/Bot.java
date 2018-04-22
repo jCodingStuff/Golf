7ba2ed81528a1b8a8066a3871d4f70a4f2e28664
kaspar <7kaspu@gmail.com>
2018-04-22 13:43:24 +0200

    Insignificant

--- core/src/com/group/golf/ai/Bot.java
+++ core/src/com/group/golf/ai/Bot.java
@@ -13,7 +13,7 @@ public class Bot {
     private final Collision collision;
 
     /**
-     * Create a new Bot instace
+     * Create a new Bot instace 
      * @param course
      * @param ball
      * @param engine
