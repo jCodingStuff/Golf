46178b5a7c5e7ab9386605d54b7b064cd05abe52
MartijnHilders <30750446+MartijnHilders@users.noreply.github.com>
2018-04-24 15:14:16 +0200

    jhk

--- core/src/com/group/golf/Physics/Physics.java
+++ core/src/com/group/golf/Physics/Physics.java
@@ -8,7 +8,7 @@ import com.group.golf.math.Function;
 
 /**
  * Created by alex_ on 21-Mar-18.
- * A class to hold the Physics engine for the Crazy Golf game
+ * A class to hold the Physics engine for the Crazy Golf gamennn
  */
 
 public class Physics {
