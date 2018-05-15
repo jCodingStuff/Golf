725812f71b1e88d25819a0a5c6c7bde27f270fa7
Julian Marrades <julian.marrades@hotmail.es>
2018-05-15 11:42:01 +0200

    Bot changes

--- core/src/com/group/golf/ai/DumBot.java
+++ core/src/com/group/golf/ai/DumBot.java
@@ -14,18 +14,31 @@ public class DumBot implements Bot {
 	
 	private final Course course;
     private final Ball ball;
-    private final Physics engine;
-    private final Collision collision;
-	
-    public DumBot(Course course, Ball ball, Physics engine, Collision collision) {
+    private Physics engine;
+    private Collision collision;
+
+    /**
+     * Create a new instance of DumBot
+     * @param course the course
+     * @param ball the ball
+     */
+    public DumBot(Course course, Ball ball) {
 		this.course = course;
 		this.ball = ball;
-		this.engine = engine;
-		this.collision = collision;
 	}
 
     @Override
+    public void setPhysics(Physics physics) {
+        this.engine = physics;
+    }
+
+    @Override
+    public void setCollision(Collision collision) {
+        this.collision = collision;
+    }
+
+    @Override
     public void makeMove() {
-        
+
     }
 }
