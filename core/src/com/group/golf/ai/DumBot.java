23423a63d34b9a2304f8b4dc54ec5b21b759718a
kaspar <7kaspu@gmail.com>
2018-05-15 11:29:18 +0200

    constructor

--- core/src/com/group/golf/ai/DumBot.java
+++ core/src/com/group/golf/ai/DumBot.java
@@ -1,5 +1,22 @@
 package com.group.golf.ai;
 
+import com.group.golf.Ball;
+import com.group.golf.Course;
+import com.group.golf.Physics.Collision;
+import com.group.golf.Physics.Physics;
+
 public class DumBot {
+	
+	private final Course course;
+    private final Ball ball;
+    private final Physics engine;
+    private final Collision collision;
+	
+    public DumBot(Course course, Ball ball, Physics engine, Collision collision) {
+		this.course = course;
+		this.ball = ball;
+		this.engine = engine;
+		this.collision = collision;
+	}
 
 }
