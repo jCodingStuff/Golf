cd33f827fb6a934912b7b45c616e31607d96503c
Julian Marrades <julian.marrades@hotmail.es>
2018-05-16 08:16:18 +0200

    Continuing individual

--- core/src/com/group/golf/genetics/Individual.java
+++ core/src/com/group/golf/genetics/Individual.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Individual {
 
     private List<double[]> genes;
+    private double[] finishCoords;
 
     public Individual(List<double[]> genes) {
         this.genes = genes;
@@ -16,5 +17,13 @@ public class Individual {
 
     public void setGenes(List<double[]> genes) {
         this.genes = genes;
+    }
+
+    public double[] getFinishCoords() {
+        return finishCoords;
+    }
+
+    public void setFinishCoords(double[] finishCoords) {
+        this.finishCoords = finishCoords;
     }
 }
