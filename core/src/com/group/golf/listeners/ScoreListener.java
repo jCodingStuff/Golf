f7b76cdd7dac328b07f93036383134d9c51694d9
lilly <lillywush@gmail.com>
2018-06-18 12:40:50 +0200

    highscore

---
+++ core/src/com/group/golf/listeners/ScoreListener.java
@@ -0,0 +1,42 @@
+package com.group.golf.listeners;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
+public class ScoreListener {
+    FileHandle scoreFile;
+
+    public ScoreListener() {
+    scoreFile =
+          //  Gdx.files.internal("scores.txt");
+        new FileHandle("scores.txt");
+    }
+
+    public void addScore(String name, int score) {
+        scoreFile.getClass();
+
+        String reader = scoreFile.readString();
+        String[] scores = reader.split(" ");
+
+        String modScore = Integer.toString(score);
+
+        int position = 1;
+        for (int i = 1; i < scores.length; i+=2) {
+            position = i;
+            if (modScore.compareTo(scores[i]) == -1) {
+                break;
+            }
+        }
+
+        String output = "";
+        for (int i = 0; i < scores.length; i++) {
+            output += scores[i] + " ";
+             if (i == position)
+                output += name + " " + modScore + " ";
+        }
+
+        scoreFile.writeString(output,false);
+
+    }
+
+}
