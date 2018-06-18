021c3361a27e97bc6a704e3479b4682af31a1edd
Julian Marrades <julian.marrades@hotmail.es>
2018-06-18 10:58:14 +0200

    Hitsound

--- core/src/com/group/golf/modes/PlayerVSBotMode.java
+++ core/src/com/group/golf/modes/PlayerVSBotMode.java
@@ -161,6 +161,7 @@ public class PlayerVSBotMode implements GameMode {
         this.bot.makeMove();
         this.landed = true;
         System.out.println("Bot moves!");
+        this.hitSound.play();
     }
 
     private void informWinner() {
