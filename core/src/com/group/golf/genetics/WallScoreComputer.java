098f74c2b4fb1e1f7a99589e6b110b09f63abd18
Chimonas <alexandroschimonas@gmail.com>
2018-06-19 15:04:11 +0200

    genetics package double -> float still need to fix collision and physics constructor

--- core/src/com/group/golf/genetics/WallScoreComputer.java
+++ core/src/com/group/golf/genetics/WallScoreComputer.java
@@ -8,7 +8,7 @@ public class WallScoreComputer implements ScoreComputer {
 
     @Override
     public void compute(Course course, Individual[] population) {
-        double[] goal = course.getGoal();
+        float[] goal = course.getGoal();
         for (int i = 0; i < population.length; i++) {
             JVector2[] landings = population[i].getLandings();
             double closestDist = Double.MAX_VALUE;
